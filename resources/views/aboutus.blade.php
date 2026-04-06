5f923e881d8d65be2e089351d9620a58e966dc2c
BilguunTu <beck.tumurbaator@gmail.com>
2026-04-07 01:11:04 +0800

    about us page

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+  <x-slot:title>
+    Welcome to Starbucks
+  </x-slot:title>
+  <div class=" flex flex-col px-11 py-9 w-full relative h-full min-w-2xs min-h-full">
+    <h1 class="border-b border-[#00643C] w-fit p-3 mb-12">About Us</h1>
+    <div class="flex flex-col  gap-14">
+      <div class="flex gap-11 ">
+        <img src="/abouteUsHome.svg" alt="">
+        <p class="font-light test-[12px] text-wrap w-1/2">Starbucks Corporation adalah sebuah perusahaan kopi dan jaringan kedai kopi global asal Amerika Serikat yang berkantor pusat di Seattle, Washington. Starbucks adalah perusahaan kedai kopi terbesar di dunia,dengan 20.336 kedai di 61 negara, termasuk 13.123 di Amerika Serikat, 1.299 di Kanada, 977 di Jepang, 793 di Britania Raya, 732 di Cina, 473 di Korea Selatan, 363 di Meksiko, 282 di Taiwan, 204 di Filipina, 164 di Thailand dan 326 di Indonesia.</p>
+      </div>
+      <div class="flex gap-11 flex-row-reverse w-fit justify-end">
+        <img src="/abouteUsHome.svg" alt="" class="z-20">
+        <p class="font-light test-[12px] text-wrap w-1/2">Starbucks Corporation adalah sebuah perusahaan kopi dan jaringan kedai kopi global asal Amerika Serikat yang berkantor pusat di Seattle, Washington. Starbucks adalah perusahaan kedai kopi terbesar di dunia,dengan 20.336 kedai di 61 negara, termasuk 13.123 di Amerika Serikat, 1.299 di Kanada, 977 di Jepang, 793 di Britania Raya, 732 di Cina, 473 di Korea Selatan, 363 di Meksiko, 282 di Taiwan, 204 di Filipina, 164 di Thailand dan 326 di Indonesia.</p>
+      </div>
+    </div>
+  </div>
+</x-layout>
